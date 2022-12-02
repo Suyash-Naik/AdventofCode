1c9016433aafee1e27c611d3e4aea44d8a928420
Suyash NAIK <snaik@lngrad201.ist.local>
2022-12-02 13:23:23 +0100

    Commenting and tkinter filedialog ease of access

--- Code1.py
+++ Code1.py
@@ -1,11 +1,14 @@
 import pandas as pd
-
-food=open("/home/snaik/Documents/AdevntofCode2022/input",'r')
+#opens a file dialog in the current folder (detailed)
+from tkinter import filedialog as fd
+filename = fd.askopenfilename() #select the input file from Advent
+#opens the file and then reads the lines
+food=open(filename,'r')
 lineFood= food.readlines()
-
-dataframe={}
-elfNum=1
-cal=0
+#data saving? may not be the most efficient
+dataframe={} #database with {elf:cal}
+elfNum=1 #index of the elf
+cal=0 #number of calories on the given elf
 for line in lineFood:
 	if line!="\n":
 		cal=cal+int(line)
@@ -13,11 +16,13 @@ for line in lineFood:
 		dataframe[elfNum]=[cal]
 		cal=0
 		elfNum+=1
-
+#dataframe magic
 pdDataframe=pd.DataFrame(dataframe)
 callist=pdDataframe.values[0]
 callist=sorted(callist,reverse=True)
+#basic ouputting
 print('first prob:')
 print(callist[0])
 print('second prob:')
 print(callist[0]+callist[1]+callist[2])
+#The end :D first advent code!
